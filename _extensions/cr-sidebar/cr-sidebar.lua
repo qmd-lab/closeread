972939dde8698afa66d73ac816f2fa685cf5a6e3
andrewpbray <andrewbray@berkeley.edu>
2023-07-20 09:14:21 -0700

    fix iteration and add has_cr_prefix function

--- _extensions/cr-sidebar/cr-sidebar.lua
+++ _extensions/cr-sidebar/cr-sidebar.lua
@@ -1,11 +1,11 @@
 --* for every Div
     --* if its a cr-sidebar div
-      -- for all blocks TODO: this method of iterating over the blocks isn't working (line 21)
-        -- if it has a cr-id
-          -- save it as body
-        -- else
-          -- save into sidebar list of blocks
-      -- end for
+      --* for all blocks
+        --* if it has a cr-id
+          --* save it as body
+        --* else
+          --* save into sidebar table of blocks
+      --* end for
       -- create new div with column and column page class
       --  - with three divs, one for each column (with middle col)
       -- return new div
@@ -13,16 +13,38 @@
 quarto.log.output("===== Sidebar Log =====")
 
 function make_sidebar_layout(div)
-  --quarto.log.output("=== next div ===")
-  --quarto.log.output("this div's classes are: ", div.classes)
-  --quarto.log.output("div.classes:includes('cr-sidebar')?", div.classes:includes("cr-sidebar"))
+  sidebar = {}
+  
   if div.classes:includes("cr-sidebar") then
-    --quarto.log.output("A sidebar div: ", div)
-    for _, block in ipairs(div) do
-    quarto.log.output("== next block ==")
-    quarto.log.output(block)
+    for k, block in pairs(div.content) do
+      --quarto.log.output("== next block ==")
+      --quarto.log.output("key: ", k)
+      --quarto.log.output(block)
+      --quarto.log.output(">>>>> ", has_cr_prefix(block))
+      if has_cr_prefix(block) then
+        body = block
+      else
+        table.insert(sidebar, block)
+      end
+    end
+    table.insert(sidebar, 1, body)
+    quarto.log.output(sidebar)
+  end
+end
+
+-- this function is fragile
+-- should be extended to find cr prefix nested deeper into the block
+function has_cr_prefix(block)
+  answer = false
+  if block.attributes ~= nil then
+    for k,v in pairs(block.attributes) do
+      if string.sub(k, 1, 2) == "cr" then
+        answer = true
+        break
+      end
     end
   end
+  return answer
 end
           
 
