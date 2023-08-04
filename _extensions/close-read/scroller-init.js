b154593931a34678568c3118b24fc86fb440de0a
James Goldie <james@dicenerd.app>
2023-08-04 19:38:47 +1000

    WIP transition works, except blue desktop

--- _extensions/close-read/scroller-init.js
+++ _extensions/close-read/scroller-init.js
@@ -8,15 +8,62 @@
 
 console.log("Initialising scrollers...")
 
-// const scroller = scrollama();
+document.addEventListener("DOMContentLoaded", () => {
+   const scroller = scrollama();
+   
+   scroller
+     .setup({
+       step: ".cr-step",
+     })
+     .onStepEnter((response) => {
+       // { element, index, direction }
 
-// scroller
-//   .setup({
-//     step: ".step",
-//   })
-//   .onStepEnter((response) => {
-//     // { element, index, direction }
-//   })
-//   .onStepExit((response) => {
-//     // { element, index, direction }
-//   });
+      console.log("Element " + response.index +
+         " scrolled IN " + response.direction + ": " +
+         response.element.textContent)
+      // console.log(response.element)
+
+      /* unfortunately i'm noticing that scrollama sometimes misses events when
+         scrolling fast. so all sticky elements need to be touched when we're
+         receiving an update */
+      
+      if (response.direction == "down") {
+         let allStickies = Array.from(
+            document.querySelectorAll(".cr-sticky, [data-cr=\"sticky\"]"));
+         let pastStickies = allStickies.slice(0, response.index + 1);
+         let futureStickies = allStickies.slice(response.index + 1);
+         
+         console.log(pastStickies.length + " back from here; " +
+            futureStickies.length + " ahead")
+   
+         pastStickies.forEach(e => e.classList.add("cr-scrolledby"));
+         futureStickies.forEach(e => e.classList.remove("cr-scrolledby"));
+         
+      }
+
+      
+
+     })
+     .onStepExit((response) => {
+       // { element, index, direction }
+       console.log("Element " + response.index +
+         " scrolled OUT " + response.direction + ": " +
+         response.element.textContent)
+      //  console.log(response.element)
+
+       if (response.direction == "up") {
+         // do the same as above, but response.index - 1
+         let allStickies = Array.from(
+            document.querySelectorAll(".cr-sticky, [data-cr=\"sticky\"]"));
+         let pastStickies = allStickies.slice(0, response.index);
+         let futureStickies = allStickies.slice(response.index);
+         
+         console.log(pastStickies.length + " back from here; " +
+            futureStickies.length + " ahead")
+   
+         pastStickies.forEach(e => e.classList.add("cr-scrolledby"));
+         futureStickies.forEach(e => e.classList.remove("cr-scrolledby"));
+       }
+
+     });
+ });
